80cd280aea46ef0fc00ccf10edb28bdeabb4c936
koutsilis1999 <kkoutsilis@gmail.com>
2022-09-04 17:16:02 +0100

    user toJSON definition

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -14,16 +14,28 @@ interface UserDoc extends mongoose.Document {
   password: string;
 }
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    toJSON: {
+      transform(doc, ret) {
+        delete ret.password;
+        delete ret.__v;
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
 
 // we dont user arrow function in order not to ovveride 'this'
 userSchema.pre("save", async function (done) {
