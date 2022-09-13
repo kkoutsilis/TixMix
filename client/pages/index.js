b5e3348ab748bd3baefcd89748d2b99a236d5d95
kkoutsilis1999 <kkoutsilis@gmail.com>
2022-09-13 22:41:18 +0100

    update index page

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,6 +1,12 @@
-const LandingPage = () => {
-  return <h1>Landing Page</h1>;
+import buildClient from "../api/build-client";
+
+const LandingPage = ({ currentUser }) => {
+  return <h1>{currentUser ? "You are signed in" : "You are not signed in"}</h1>;
 };
 
-LandingPage.getInitialProps = () => {};
+LandingPage.getInitialProps = async (context) => {
+  const client = buildClient(context);
+  const { data } = await client.get("/api/users/currentuser");
+  return data;
+};
 export default LandingPage;
