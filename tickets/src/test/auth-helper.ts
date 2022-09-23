0ed077e62db2d9986728fe644905e2cc7bd8bbe2
Konstantinos Koutsilis <kkoutsilis@gmail.com>
2022-09-23 22:06:45 +0100

    update ticket  auth helper

--- tickets/src/test/auth-helper.ts
+++ tickets/src/test/auth-helper.ts
@@ -1,15 +1,10 @@
-import request from "supertest";
-import { app } from "../app";
+import jwt from "jsonwebtoken";
 
 export const getAuthCookie = async () => {
-  const email = "test@test.com";
-  const password = "password";
-
-  const response = await request(app)
-    .post("/api/users/signup")
-    .send({ email, password })
-    .expect(201);
-
-  const cookie = response.get("Set-Cookie");
-  return cookie;
+  const payload = { id: "a131sdfs3e", email: "test@test.com" };
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+  const session = { jwt: token };
+  const sessionJSON = JSON.stringify(session);
+  const base64 = Buffer.from(sessionJSON).toString("base64");
+  return [`session=${base64}`];
 };
