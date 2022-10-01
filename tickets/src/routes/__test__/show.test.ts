e6ee6550cebb8147e051f8d6008253b30dc8d471
Konstantinos Koutsilis <kkoutsilis@gmail.com>
2022-10-01 14:58:58 +0100

    show tickets route

---
+++ tickets/src/routes/__test__/show.test.ts
@@ -0,0 +1,29 @@
+import request from "supertest";
+import { app } from "../../app";
+import { Ticket } from "../../models/ticket";
+import mongoose from "mongoose";
+
+it("should return 404 if ticket does not exist", async () => {
+  const id = new mongoose.Types.ObjectId().toHexString();
+  await request(app).get(`/api/tickets/${id}`).send().expect(404);
+});
+
+it("should return the ticket if it exists", async () => {
+  const title = "sometitle";
+  const price = 10;
+  const userId = "someid";
+  const ticket = Ticket.build({
+    title,
+    price,
+    userId,
+  });
+  ticket.save();
+
+  const response = await request(app)
+    .get(`/api/tickets/${ticket.id}`)
+    .send()
+    .expect(200);
+
+  expect(response.body.title).toEqual(title);
+  expect(response.body.price).toEqual(price);
+});
