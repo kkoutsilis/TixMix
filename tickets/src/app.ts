d64e4fc7a92b2a5d2bfd08b2c5aa0a381ec85431
Konstantinos Koutsilis <kkoutsilis@gmail.com>
2022-10-01 15:12:58 +0100

    index ticker router

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -6,6 +6,7 @@ import { errorHandler, NotFoundError, currentUser } from "@ktixmix/common";
 
 import { createTicketRouter } from "./routes/new";
 import { showTicketRouter } from "./routes/show";
+import { indexTicketRouter } from "./routes/intex";
 
 const app = express();
 app.set("trust proxy", true);
@@ -20,6 +21,7 @@ app.use(currentUser);
 
 app.use(createTicketRouter);
 app.use(showTicketRouter);
+app.use(indexTicketRouter);
 
 app.all("*", async (res, req) => {
   throw new NotFoundError();
---
+++ tickets/src/routes/__test__/index.test.ts
@@ -0,0 +1,20 @@
+import request from "supertest";
+import { app } from "../../app";
+import { getAuthCookie } from "../../test/auth-helper";
+
+const createTicket = async () => {
+  return request(app)
+    .post("/api/tickets")
+    .set("Cookie", await getAuthCookie())
+    .send({ title: "sometitle", price: 10 });
+};
+
+it("can fetch a list of tickets", async () => {
+  await createTicket();
+  await createTicket();
+  await createTicket();
+
+  const response = await request(app).get("/api/tickets").send().expect(200);
+
+  expect(response.body.length).toEqual(3);
+});
