60e165d2b583d193867fea47f9e09a59f155c7a1
Konstantinos Koutsilis <kkoutsilis@gmail.com>
2022-10-18 21:25:09 +0100

    delete route initial impl

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,9 +1,32 @@
 import express, { Request, Response } from "express";
+import { Order, OrderStatus } from "../models/order";
+import {
+  requireAuth,
+  NotFoundError,
+  NotAuthorizedError,
+} from "@ktixmix/common";
 
 const router = express.Router();
 
-router.delete("/api/orders/:orderId", async (req: Request, res: Response) => {
-  res.send({});
-});
+//technically shoud be a patch
+router.delete(
+  "/api/orders/:orderId",
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const { orderId } = req.params;
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+    if (order.userId != req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+
+    order.status = OrderStatus.Cancelled;
+    await order.save();
+    res.status(204).send(order);
+  }
+);
 
 export { router as deleteOrderRouter };
