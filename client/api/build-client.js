c1c49278b394ae33f7e477172ebc472811bdb7be
Konstantinos Koutsilis <kkoutsilis@gmail.com>
2022-11-06 19:17:57 +0000

    update host config

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -4,8 +4,7 @@ export default ({ req }) => {
   if (typeof window === "undefined") {
     //on the server
     return axios.create({
-      baseURL:
-        "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
+      baseURL: "http://www.tixmix.store/",
       headers: req.headers,
     });
   } else {
