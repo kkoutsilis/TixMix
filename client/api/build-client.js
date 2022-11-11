d16fd144d4f11d35d17908e27a63ab64a0e47b3b
Konstantinos Koutsilis <kkoutsilis@gmail.com>
2022-11-11 23:01:37 +0000

    update orders list

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -4,7 +4,9 @@ export default ({ req }) => {
   if (typeof window === "undefined") {
     //on the server
     return axios.create({
-      baseURL: "http://www.tixmix.store/",
+      // baseURL: "http://www.tixmix.store/",
+      baseURL:
+        "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
       headers: req.headers,
     });
   } else {
