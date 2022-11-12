e21accfc2e95fa1a92881de5b324cee51e87ea0c
Konstantinos Koutsilis <kkoutsilis@gmail.com>
2022-11-12 09:10:45 +0000

    change baseurl

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -4,9 +4,9 @@ export default ({ req }) => {
   if (typeof window === "undefined") {
     //on the server
     return axios.create({
-      // baseURL: "http://www.tixmix.store/",
-      baseURL:
-        "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
+      baseURL: "http://www.tixmix.store/",
+      // baseURL:
+      //   "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
       headers: req.headers,
     });
   } else {
