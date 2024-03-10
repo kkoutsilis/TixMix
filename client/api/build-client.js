1f51f0fe37facab0ddafa2001d197e40c74bde2c
Konstantinos Koutsilis <kkoutsilis@gmail.com>
2024-03-10 13:27:27 +0000

    Fix build-client.js

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -3,19 +3,14 @@ import axios from "axios";
 export default ({ req }) => {
   if (typeof window === "undefined") {
     //on the server
-    // TODO: Probably need to improve that,
-    // but this is a future Konstantinos problem :) 
-    if (process.env.APP_ENV.toLowerCase() === "development") {
-      return axios.create({
-        baseURL:
-          "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
-        headers: req.headers,
-      });
-    } else {
-      return axios.create({
-        baseURL: "http://www.tixmix.store/",
-      });
-    }
+    const isDevelopment = process.env.APP_ENV.toLowerCase() === 'development';
+    const baseURL = isDevelopment
+      ? 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local'
+      : 'http://www.tixmix.store';
+    return axios.create({
+      baseURL,
+      headers: req.headers,
+    });
   } else {
     return axios.create({ baseURL: "/" });
   }
